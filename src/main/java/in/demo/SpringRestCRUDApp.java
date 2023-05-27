86e7c3ce552fac4c8f4e733dad967d8e67906226
Ramya RK <Ramya.Kumar@unisys.com>
2023-05-28 00:15:05 +0530

    First commit

---
+++ src/main/java/in/demo/SpringRestCRUDApp.java
@@ -0,0 +1,12 @@
+package in.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCRUDApp{
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestCRUDApp.class, args);
+	}
+}
